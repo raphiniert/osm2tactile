7bc4171a953fe8c98fdb24dbd21bad3b3a532a0d
raphiniert <dev@raphiniert.eu>
2022-06-16 09:17:09 +0200

    implemented very basic flask app, displaying the  mapnik version

---
+++ osm2tactile/__init__.py
@@ -0,0 +1,50 @@
+import os
+import logging
+
+from flask import current_app, g, Flask, request
+from rich.logging import RichHandler
+
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="DEBUG", format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S", handlers=[RichHandler()]
+)
+
+logger = logging.getLogger("osm2tactile")
+
+
+def create_app(test_config=None):
+    """
+    # TODO: comment create_app
+    :param test_config:
+    :return:
+    """
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        config_file = "config.py"
+        app.config.from_pyfile(config_file, silent=True)
+        logger.debug(f"Loaded app conifg from {config_file}.")
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+        logger.debug(f"Loaded app conifg from mapping {test_config}.")
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # register blueprints
+    from osm2tactile import osm2tactile
+
+    app.register_blueprint(osm2tactile.bp)
+    logger.debug(f"Registered osm2tactile blueprint")
+
+    app.add_url_rule("/", endpoint="root", view_func=osm2tactile.index)
+    logger.debug(f"Added url rule for / as root endpoint to osm2tactile.index")
+    
+    return app
---
+++ osm2tactile/osm2tactile.py
@@ -0,0 +1,15 @@
+import logging
+import requests
+import mapnik
+
+from flask import current_app, g, Blueprint, render_template, request
+
+
+logger = logging.getLogger("osm2tactile")
+
+bp = Blueprint("osm2tactile", __name__)
+
+
+@bp.route("/", methods=("GET",), strict_slashes=False)
+def index():    
+    return render_template(f"osm2tactile/index.html", mapnik_version=mapnik.mapnik_version_string())

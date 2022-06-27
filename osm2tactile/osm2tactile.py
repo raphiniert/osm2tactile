a840f0e182133f1f4deda65f0229f02edd14c293
raphiniert <dev@raphiniert.eu>
2022-06-27 11:59:58 +0200

    reformatted files w/ black

--- osm2tactile/osm2tactile.py
+++ osm2tactile/osm2tactile.py
@@ -1,9 +1,11 @@
-from cgitb import text
 import logging
 import requests
 import mapnik
 
+from sqlalchemy import func
 from flask import current_app, g, Blueprint, render_template, request
+
+from osm2tactile.models import OSMNodes, OSMLine, OSMPoint, OSMPolygon
 
 
 logger = logging.getLogger("osm2tactile")
@@ -63,10 +65,70 @@ def index():
     map_file_name = f"{lat}_{lon}_{dx}_{dy}.png"
     mapnik.render_to_file(m, f"{static_path}/{map_path}/{map_file_name}")
 
+    # query street names (lines that are highways), intersecting the bounding box
+    envelope = [
+        bounding_box.minx,
+        bounding_box.miny,
+        bounding_box.maxx,
+        bounding_box.maxy,
+    ]
+    streets = (
+        OSMLine.query.with_entities(OSMLine.name)
+        .filter(OSMLine.way.op("&&")(func.ST_MakeEnvelope(*envelope)))
+        .filter(OSMLine.highway != None)
+        .filter(OSMLine.name != None)
+        .group_by(OSMLine.name)
+        .all()
+    )
+
+    # query points of interest and nearest steet for guessing the street
+    points = (
+        OSMPoint.query.filter(OSMPoint.way.op("&&")(func.ST_MakeEnvelope(*envelope)))
+        .filter(OSMPoint.name != None)
+        .all()
+    )
+    # TODO: make this a function or template fiter to get street name
+    point_lines = {}
+    for point in points:
+        line = (
+            OSMLine.query.with_entities(
+                OSMLine.name,
+                func.ST_Distance(
+                    func.ST_LineInterpolatePoint(
+                        OSMLine.way, func.ST_LineLocatePoint(OSMLine.way, point.way)
+                    ),
+                    point.way,
+                ).label("distance"),
+            )
+            .filter(OSMLine.way.op("&&")(func.ST_MakeEnvelope(*envelope)))
+            .filter(OSMLine.highway != None)
+            .filter(OSMLine.name != None)
+            .order_by("distance")
+            .first()
+        )
+        point_lines[point.osm_id] = line[0]
+
+    # TODO: query public transport stations
+
+    # query buildings
+    buildings = (
+        OSMPolygon.query.filter(
+            OSMPolygon.way.op("&&")(func.ST_MakeEnvelope(*envelope))
+        )
+        .filter(OSMPolygon.building != None)
+        .all()
+    )
+
     return render_template(
         f"osm2tactile/index.html",
         mapnik_version=mapnik.mapnik_version_string(),
         map_img=f"{map_path}/{map_file_name}",
         lat=lat,
         lon=lon,
+        dx=dx,
+        dy=dy,
+        streets=streets,
+        points=points,
+        point_lines=point_lines,
+        buildings=buildings,
     )

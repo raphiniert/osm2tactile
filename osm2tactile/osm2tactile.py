37b8a3012418d0adef39ca2f1fdb493a5baf4fce
raphiniert <dev@raphiniert.eu>
2022-06-28 13:16:06 +0200

    added building layer

--- osm2tactile/osm2tactile.py
+++ osm2tactile/osm2tactile.py
@@ -27,9 +27,15 @@ def get_mapnik_stylesheet():
             "size": 16,
         },
     }
+    building = {
+        "line": {"stroke": "#dddddd", "stroke_width": 3, "stroke_dasharray": "10,10"}
+    }
 
     return render_template(
-        "xml/mapnik_stylesheet.xml", background_color=background_color, road=road
+        "xml/mapnik_stylesheet.xml",
+        background_color=background_color,
+        road=road,
+        building=building,
     )
 
 
@@ -62,6 +68,7 @@ def index():
 
     # make the style available to the map with a name
     road_style_name = "road-style"
+    building_style_name = "building-style"
 
     # create layer containing the actual map data
     roads_layer = mapnik.Layer("roads-layer")
@@ -75,9 +82,22 @@ def index():
     # append a certain style to the layer
     roads_layer.styles.append(road_style_name)
 
+    # create layer containing the actual map data
+    building_layer = mapnik.Layer("roads-layer")
+    building_layer.datasource = mapnik.PostGIS(
+        host=current_app.config["POSTGRES_HOST"],
+        user=current_app.config["POSTGRES_USER"],
+        password=current_app.config["POSTGRES_PASSWORD"],
+        dbname=current_app.config["POSTGRES_DB"],
+        table="planet_osm_polygon",
+    )
+    # append a certain style to the layer
+    building_layer.styles.append(building_style_name)
+
     # append the layer to the map
     mapnik.load_map_from_string(m, get_mapnik_stylesheet())
     m.layers.append(roads_layer)
+    m.layers.append(building_layer)
 
     # save-path for the image
     static_path = "osm2tactile/static"
